6f613f55f71612394618ef7d4ddbcd932e4513d8
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-09-08 13:39:54 +0800

    line follow module

--- missions/lineFollowModule.py
+++ missions/lineFollowModule.py
@@ -3,13 +3,21 @@ import cv2
 import numpy as np
 import time
 import keyPressModule as kp
+from imageRecolorModule import colorize
 
 kp.init()
 
 DRONECAM = True  # using drone or computer cam
 
-w, h = 321, 240  # width and height of video frame
-hsvVals = [0, 0, 255, 0, 0, 255]  # hsv range values for yellow line
+FRONTCAM = True
+
+if FRONTCAM:
+    w, h = 360, 240  # width and height of video frame
+else:
+    w, h = 321, 240  # width and height of video frame
+
+hsvVals = [0, 36, 179, 81, 255, 255]  # hsv range values for yellow line
+thres_vals = [181, 191, 188, 255, 255, 255]  # threshold range values for white line
 
 sensors = 3  # sensors in the image
 
@@ -26,7 +34,11 @@ if DRONECAM:
     me = tello.Tello()
     me.connect()
     time.sleep(1)
-    me.streamon_bottom()
+    if FRONTCAM:
+        me.streamon_front()
+    else:
+        me.streamon_bottom()
+
     time.sleep(3)
     print(me.get_battery())
 else:
@@ -34,18 +46,20 @@ else:
 
 me.send_rc_control(0, 0, 0, 0)
 me.takeoff()
-me.send_rc_control(0, 0, 0, 0)
-
+time.sleep(5)
 
 # move to set height above the ground
-# flightHeight = 20  # fly at this level above the ground
-# me.send_rc_control(0, 0, flightHeight-me.get_height(), 0)
-# while True:
-#     print(me.get_height())
-#     if me.get_height() == flightHeight:
-#         me.send_rc_control(0, 0, 0, 0)
-#         break
-# print("height is: {}".format(me.get_height()))
+flightHeight = 20  # fly at this level above the ground
+goToHeight = flightHeight - me.get_height()
+while True:
+    me.send_rc_control(0, 0, goToHeight, 0)
+    print(me.get_height())
+    if me.get_height() == flightHeight:
+        me.send_rc_control(0, 0, 0, 0)
+        break
+
+print("height is: {}".format(me.get_height()))
+
 
 def thresholding(img):
     """
@@ -57,6 +71,18 @@ def thresholding(img):
     lower = np.array([hsvVals[0], hsvVals[1], hsvVals[2]])
     upper = np.array([hsvVals[3], hsvVals[4], hsvVals[5]])
     mask = cv2.inRange(hsv, lower, upper)
+    return mask
+
+
+def thresholding_bw(img):
+    """
+    thresholding function, find the target white patch from the black and white image
+    :param img: black and white image
+    :return: mask, thresholded image
+    """
+    lower = np.array([thres_vals[0], thres_vals[1], thres_vals[2]])
+    upper = np.array([thres_vals[3], thres_vals[4], thres_vals[5]])
+    mask = cv2.inRange(img, lower, upper)
     return mask
 
 
@@ -115,7 +141,7 @@ def sendCommands(senOut, cx):
     print(lr)
     lr = int(np.clip(lr, -100, 100))
 
-    if lr < 20 and lr > -20:
+    if lr < 2 and lr > -2:
         lr = 0
 
     # Rotation
@@ -155,9 +181,10 @@ def followLine(tello):
         if DRONECAM:
             img = tello.get_frame_read().frame
 
-            img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+            if not FRONTCAM:
+                img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+                cv2.imshow("output-0", img)
 
-            cv2.imshow("output-0", img)
             print("drone cam")
             gotStream = True
         else:
@@ -167,9 +194,23 @@ def followLine(tello):
         if gotStream:
             img = cv2.resize(img, (w, h))
 
-            # img = img_cut = img[(img.shape[0] - img.shape[0] // 4):, :, :]
-            imgThres = thresholding(img)
-            cx = getContours(imgThres, img)  ## image translation
+            if FRONTCAM:
+                img = img_cut = img[(img.shape[0] - img.shape[0] // 4):, :, :]
+                imgThres = thresholding(img)  # color image thresholding
+            else:
+                colorized = colorize(img)
+                imgThres = thresholding(img)  # color image thresholding
+                # imgThres = thresholding_bw(img)  # black and white image thresholding
+
+            cx = getContours(imgThres, img)  # image translation
+
+            if cx == 0:  # if no contour found, reached end of line
+                print("Reached end of line")
+                me.send_rc_control(0, fspeed, 0, 0)
+                time.sleep(2)
+                me.land()
+                break
+
             senOut = getSensorOutput(imgThres, sensors)
             sendCommands(senOut, cx)
             cv2.imshow("output", img)
