7eb7c9a283c57bc4786887d64f0924cfc24e4594
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-09-10 12:12:40 +0800

    obstacle avoidance mission

--- missions/obstacle_avoidance_mission.py
+++ missions/obstacle_avoidance_mission.py
@@ -1,9 +1,41 @@
+import keyPressModule as kp
+
 def init(tello):
     """
         initializing the obstacle avoidance, should be called first before calling
         trackObject()
     """
     print("obstacle avoidance initializing...")
+
+    if tello.is_flying is False:
+        raise Exception('drone is not flying, can\'t start mission')
+
+        # move to set height above the ground
+    flight_height = 80  # fly at this level above the ground
+
+    curr_height = tello.get_height()
+
+    go_to_height_v = 0  # velocity for going to mission flight height
+    if (flight_height - curr_height) > 0:
+        go_to_height_v = 15
+    else:
+        go_to_height_v = -15
+
+    while True:
+        if kp.getKey("q"):  # Allow press 'q' to land in case of emergency
+            tello.land()
+
+        curr_height = tello.get_height()
+
+        print(f'flying at {curr_height}cm')
+
+        if curr_height == flight_height:
+            tello.send_rc_control(0, 0, 0, 0)
+            break
+
+        tello.send_rc_control(0, 0, go_to_height_v, 0)
+
+    print("Reached obstacle avoidance mission height of: {} cm".format(tello.get_height()))
 
 
 def avoidObstacles(tello):
