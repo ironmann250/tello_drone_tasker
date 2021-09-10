f4938118e02e36e708df2a483c3a4c2658a888ae
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-09-10 12:38:08 +0800

    Update line_follow_mission.py

--- missions/line_follow_mission.py
+++ missions/line_follow_mission.py
@@ -294,7 +294,7 @@ if __name__ == '__main__':
             tello.streamon_bottom()
 
         time.sleep(3)
-        print(tello.get_battery())
+        print("battery level is {}".format(me.get_battery()))
     else:
         cap = cv2.VideoCapture(0)
 
