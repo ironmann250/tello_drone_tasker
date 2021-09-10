e0ce20052d4edef270c1f5c751c357da301a968f
ironmann350 <ironmann350@outlook.com>
2021-09-10 14:11:21 +0800

    Update obstacle_avoidance_mission.py

--- missions/obstacle_avoidance_mission.py
+++ missions/obstacle_avoidance_mission.py
@@ -47,7 +47,6 @@ def avoidObstacles(tello):
         initializing the obstacle avoidance, should be called after calling
         init()
     """
-    tello.land()
     print("obstacle avoidance launched...")
 
 
@@ -72,8 +71,8 @@ if __name__ == "__main__":
 
     tello.send_rc_control(0, 0, 0, 0)
 
-    #tello.takeoff()
-    #time.sleep(3)
+    tello.takeoff()
+    time.sleep(3)
 
     init(tello)
     avoidObstacles(tello)
